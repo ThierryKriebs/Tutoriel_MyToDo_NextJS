a4faa8c9591b87b0413dcc76d36a56f16be3fac8
ThierryKriebs <kriebs.thierry@gmail.com>
2026-08-23 16:15:51 +0200

    Exercice 73 background image terminé

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Background from "./ui/Background";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <header>LOGO</header>
         <main>
           {children}
+          <Background />
         </main>
         <footer><p>&copy;DonkeyGeek</p></footer>
       </body>  
---
+++ app/ui/Background.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+
+export default function Background() {
+  return (
+    <Image
+        alt="Arrière-plan avec neige"
+        src="/christmas-background.jpg"
+        quality={100}
+        fill  //={true} par défaut =>remplit l'élément parent
+        // sizes="100vw" //occupe toute la largeur de la fenêtre du navigateur
+        style= {{
+            objectFit:'cover', //redimentionnement en remplissant le parent tout en respectant le ratio tout 
+            // paddingTop: '50px',
+            zIndex: '-5'
+        }}
+    />
+  )
+}
