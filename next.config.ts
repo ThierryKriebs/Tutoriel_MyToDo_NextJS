698dc50090c662d42466165bd4b95f24721b829d
ThierryKriebs <kriebs.thierry@gmail.com>
2026-08-23 13:00:16 +0200

    Exercice 72 afficher Image externe terminée

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  // config pour les images externes
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        pathname: '/**', //Autoriser toutes les images sous ce domaine
+      },
+    ]
+  }
 };
 
 export default nextConfig;
