e5bcc7d2620309b9d3a8beddc6f682135956b4c7
ThierryKriebs <kriebs.thierry@gmail.com>
2026-08-24 21:28:18 +0200

    Leçon 78 formulaire créer une tâche, terminé

--- app/create-todo/page.tsx
+++ app/create-todo/page.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+'use client'
 
-export default function page() {
+import { useState } from "react"
+
+
+const CreateTodo = () => {
+
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
+  const [isDisabled, setIsDisabled] = useState(false)
+
+  const handleCreateTodo = () => {
+
+  }
+
   return (
-    <div><h1>Create todo</h1></div>
+    <>
+      <form className="form" onSubmit={handleCreateTodo}>
+        <div className = "title">
+          <h1>Créer une tâche</h1>
+        </div>
+        <div className="align-horizontal">
+          <div className="todo-container">
+            <label className="placeholder">Tâche</label>
+            <input
+              className="input"
+              type="text"
+              value={title}
+              placeholder="Indiquez une tâche"
+              autoComplete="off"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+
+          <div className="date-container">
+            <label className="placeholder">Date</label>
+            <input
+              className="input"
+              type="date"
+              value={date}
+              placeholder="Indiquez une date"
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="button-container">
+          {
+            !isDisabled && <button type="submit" className="btn-success">Créer</button>
+          }
+        </div>
+      </form>
+    </>
   )
 }
+
+export default CreateTodo
