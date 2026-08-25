217179f06dbe6d169e24b713ba451e7e07279ff3
ThierryKriebs <kriebs.thierry@gmail.com>
2026-08-25 21:47:42 +0200

    Exercice 82 lire des données de MongoDB terminé

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,5 +1,45 @@
-export default function page() {
+"use client"
+import { useState, useEffect } from "react"
+
+interface Todo {
+  id: string;
+  title: string;
+  date: string;
+}
+
+const TodoList = () => {
+  const [todos, setTodos] = useState<Todo[]>([]); //ce state est un array de Todo
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    const getTodos = async () => {
+      const response = await fetch("api/todos");
+      const data = await response.json();
+      console.log(data);
+      setTodos(data);
+      setIsLoading(false);
+    }
+
+    getTodos();
+  }, [])
+
   return (
-    <div>page</div>
+    <section>
+      <h1 className="text-center">Tâches créés</h1>
+      {
+        todos.length === 0 && (
+          <p className="loader">
+            {isLoading ? "Veuillez patienter ..." : "Aucune Todos à afficher"}
+          </p>
+        )
+      }
+
+      <div className="listContainer">
+        {/* LIST */}
+      </div>
+
+    </section>
   )
 }
+
+export default TodoList
